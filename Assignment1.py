c8c58e119f65cbae0509359b97eae40391a0585e
Niva Chitrakar <nivachitrakar9@gmail.com>
2020-07-01 12:27:40 -0400

    Python Assignment

---
+++ Assignment1.py
@@ -0,0 +1,28 @@
+# Question 1
+a,b,c = 24,1209.94,"python"
+print (a,b,c)
+
+# Question 2
+d = (12+3j)
+print (d)
+
+# Question 3
+a,d = d,a
+print(a)
+print(d)
+
+#Question 4
+i = eval(input("Enter any value: "))
+print(i)
+
+# Question 5
+x,y=eval(input("Enter any two numbers between 1-10: "))
+### 5.1
+z = x+y
+### 5.2
+q = z+30
+print(q)
+
+# Question 6
+a6 = eval(input("Enter any value: "))
+print("the datatype for entered value is: ",type(a6))
